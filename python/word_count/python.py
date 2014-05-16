69de002a9e8ef841777ea2bcc800bf5ceca24a19
Pavel Kulikov <kulikovpavel@gmail.com>
2014-05-16 20:49:07 +0400

    'wordcount'

---
+++ python/word_count/python.py
@@ -0,0 +1,17 @@
+with open('book1.txt', 'r', encoding='utf-8') as file:
+	list = file.readlines()
+	
+	dict = {}
+	for elem in list:
+		words = elem.lower().replace(".", "").replace(",", "").replace(";", "").replace(":", "").replace("-", "").split(" ")
+		for word in words:
+			#if word == '\n': continue
+			if word in dict:
+				dict[word] += 1
+			else: 
+				dict[word] = 1
+
+	with open('output.txt', 'w', encoding='utf-8') as file:
+		for w in sorted(dict, key=dict.get, reverse=True):  #получаем сортированный по значению список ключей, после чего записываем в файл
+			file.write('%s %s\n' % (w, dict[w]))
+		 

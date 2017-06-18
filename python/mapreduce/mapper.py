44d14c88c13d065f2199056c2ede28f5a2775163
Pavel Kulikov <kulikovpavel@gmail.com>
2017-06-18 18:24:16 +0300

    Update mapper.py

--- python/mapreduce/mapper.py
+++ python/mapreduce/mapper.py
@@ -1,1 +1,11 @@
+import sys
+in_file = sys.stdin.readlines()
 
+
+data = map(lambda x: x.split('\t'), in_file)
+
+for line in data:
+    words = ''.join(filter(lambda x: x.isalpha() or x.isspace(), line[1])).lower().split()
+
+    for word in set(words):
+        print(word + '\t' + line[0])
